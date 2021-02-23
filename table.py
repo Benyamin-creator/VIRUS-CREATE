befbee774e95aaaa85a96c8daf68bf5ec0893e74
BENYAMIN <68799057+Benyamin-creator@users.noreply.github.com>
2021-02-23 02:35:34 -0500

    Update table.py

--- table.py
+++ table.py
@@ -8,9 +8,8 @@ G = '\033[32m' # Green
 O = '\033[0;33m' # Orange
 B = '\033[1;34m' #Blue
 def force_to_unicode(text): 
-  
-"If text is unicode, it is returned as is. If it's str, convert it to Unicode using UTF-8 encoding" 
-  return text if isinstance(text, unicode) else text.decode('utf8')
+        "If text is unicode, it is returned as is. If it's str, convert it to Unicode using UTF-8 encoding" 
+        return text if isinstance(text, unicode) else text.decode('utf8')
 
 print
 print (""+R+"          ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
